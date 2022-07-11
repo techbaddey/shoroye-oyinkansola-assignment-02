02c4f1bf261e4c0b2beb5b39981ce2c15f6acc86
Oyinkansola Shoroye <74302397+teqbaddie@users.noreply.github.com>
2022-07-11 17:54:11 +0000

    i used for loop to loop through the elements of the array I changed the return value from to sum in challenge 1 and I changed the return value to count in challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,12 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+ let sum = 0
+  for(let array of arrayOfNumbers)
+    sum += array;
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +31,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let count = 0
+  for (const array of arrayOfNumbers) {
+    if (array % 2 === 0) {
+      count++;
+    }
+  }
+    return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
